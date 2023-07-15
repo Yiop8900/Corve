e27e1c5186bedf04668bfe267d2bd1eef665a345
yiop8900 <veroizaluis@gmail.com>
2023-07-15 17:18:30 -0400

    v03

---
+++ js/extras.js
@@ -0,0 +1,4 @@
+function openGmailApp() {
+    var url = "https://mail.google.com/mail/?extsrc=mailto&url=mailto:veroizaluis@gmail.com?subject=Solicitud de Transporte&body=Cuerpo del mensaje";
+    window.open(url, "_blank");
+  }
