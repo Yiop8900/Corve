23d59b833cc11d3e4ebf39b5804932ee28c3c093
yiop8900 <veroizaluis@gmail.com>
2023-07-15 17:25:48 -0400

    v04

--- js/extras.js
+++ js/extras.js
@@ -1,4 +1,18 @@
 function openGmailApp() {
-    var url = "https://mail.google.com/mail/?extsrc=mailto&url=mailto:veroizaluis@gmail.com?subject=Solicitud de Transporte&body=Cuerpo del mensaje";
-    window.open(url, "_blank");
+    var email = "veroizaluis@gmail.com";
+    var subject = "Solicitud de Transporte";
+    var body = "Cuerpo del mensaje";
+  
+    var userAgent = navigator.userAgent.toLowerCase();
+    var isMobile = /mobile|tablet|ip(ad|hone|od)|android|silk/i.test(userAgent);
+  
+    if (isMobile) {
+      var url = "googlegmail://co?to=" + email + "&subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+      window.location.href = url;
+    } else {
+      var mailtoUrl = "mailto:" + email + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+      window.location.href = mailtoUrl;
+    }
   }
+  
+  document.getElementById("escribenos-link").addEventListener("click", openGmailApp);
